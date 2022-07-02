ac66644c65803aa0e7e6044abc4f83128497ef9c
sriram <97436480+ram1494@users.noreply.github.com>
2022-07-03 00:49:43 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(j=1;j<=n;j++)     {         for(i=n;i>0;i--)         {             printf("%d ",i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(j=1;j<=n;j++)
     {
-        for(j=1;j<=n;j++)
+        for(i=n;i>0;i--)
         {
-            if(i==j||j==1||i==n)
-            {
-                printf("*");
-            }
-            else
-            {
-                printf(" ");
-            }
+            printf("%d ",i);
         }
         printf("
 ");
